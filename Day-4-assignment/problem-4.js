883c9dd6ca62319aff2cdd6adc2c9483ce141460
unknown <abhisinchit49@gmail.com>
2022-09-17 01:45:04 +0530

    day 4 assignment added

---
+++ Day-4-assignment/problem-4.js
@@ -0,0 +1,12 @@
+// Problem 4: Given 3 numbers (all different values), print which is greatest
+
+let a = 52;
+let b = 810;
+let c = 155;
+
+if (a > b && a > c) {
+  console.log(a)
+} else if (b > a && b > c) {
+  console.log(b)
+} else
+  console.log(c)
